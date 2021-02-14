9fe2159fc01567847bcff0f4310ad3b878fb5f84
Pratik Sah <shivam3448@gmail.com>
2021-02-14 21:10:14 +0530

    Added data files

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,7 +3,23 @@ import 'package:learning/models/catelog.dart';
 import 'package:learning/widgets/drawer.dart';
 import 'package:learning/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() {
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(30, (index) => CatelogModel.items[0]);
